ca60da11fd76a9d34a980f541228c1d06772eaa1
Yakiv <sapa.yakiv@gmail.com>
2026-05-12 16:53:56 +0200

    Refactor CheckVerifiedUserSubscriber to include email in redirect response for unverified user login

--- src/EventSubscriber/CheckVerifiedUserSubscriber.php
+++ src/EventSubscriber/CheckVerifiedUserSubscriber.php
@@ -3,21 +3,21 @@ namespace App\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Event\CheckPassportEvent;
-use Symfony\Component\Security\Http\Authenticator\Passport\UserPassportInterface;
 Use App\Entity\User;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use App\Security\AccountNotVerifiedAuthenticationException;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Http\Event\LoginFailureEvent;
+use Symfony\Component\Security\Http\SecurityRequestAttributes;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class CheckVerifiedUserSubscriber implements EventSubscriberInterface
 {
     private RouterInterface $router;
-    public function __construct(RouterInterface $router)
+    public function __construct(RouterInterface $router, private RequestStack $requestStack)
     {
         $this->router = $router;
+        $this->requestStack = $requestStack;
     }
     public function onCheckPassport(CheckPassportEvent $event)
     {
@@ -36,7 +36,10 @@ class CheckVerifiedUserSubscriber implements EventSubscriberInterface
         if (!$event->getException() instanceof AccountNotVerifiedAuthenticationException) {
             return;
         }
-        $response = new RedirectResponse($this->router->generate('app_verify_resend_email'));
+        $request = $event->getRequest();
+        $email = $request->getSession()->get(SecurityRequestAttributes::LAST_USERNAME);
+
+        $response = new RedirectResponse($this->router->generate('app_verify_resend_email', ['email' => $email]));
         $event->setResponse($response);
     }
     public static function getSubscribedEvents()
