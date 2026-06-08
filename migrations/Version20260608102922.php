3d47b61033d6b085698a0e8b1f2fa96b557f62f5
Yakiv <sapa.yakiv@gmail.com>
2026-06-08 13:03:22 +0200

    Add migration to include displayName column in user table

---
+++ migrations/Version20260608102922.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+
+final class Version20260608102922 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add displayName column to user table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE user ADD displayName VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE user DROP displayName');
+    }
+}
