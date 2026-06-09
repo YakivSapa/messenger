098062ceb1de979b48c13eb605805a90a0cc2ad1
Yakiv <sapa.yakiv@gmail.com>
2026-06-09 13:13:21 +0200

    Enhance user settings form to update display name and add flash messages for success/error feedback

--- src/Controller/UserSettingsController.php
+++ src/Controller/UserSettingsController.php
@@ -6,24 +6,48 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\SecurityBundle\Security;
+use App\Form\GeneralUserSettingsType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
 
 final class UserSettingsController extends AbstractController
 {
     #[Route('/user/settings/{section}', name: 'app_user_settings')]
-    public function index(Security $security, string $section = 'initial'): Response
+    public function index(Security $security, Request $request, EntityManagerInterface $entityManager, string $section = 'initial'): Response
     {
-        $user = $security->getUser();
-        if (!$user) {
+        /**
+         * @var User $user
+         */
+        $user = $this->getUser();
+        $currentUser = $security->getUser();
+        $form = $this->createForm(GeneralUserSettingsType::class);
+        if (!$currentUser) {
             throw $this->createAccessDeniedException('You must be logged in to access this page.');
         }
         $allowedSections = ['initial', 'general', 'password-reset'];
         if (!in_array($section, $allowedSections)) {
             throw $this->createNotFoundException('The requested settings section does not exist.');
         }
+        if ($section === 'general') {
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                // dd($form->getData());
+                $displayName = $form->get('displayName')->getData();
+                $user->setDisplayName($displayName);
+                $entityManager->persist($user);
+                $entityManager->flush();
+                $this->addFlash('success', 'Your settings have been updated successfully.');
+                return $this->redirectToRoute('app_user_settings', ['section' => 'general']);
+            } else if ($form->isSubmitted() && !$form->isValid()) {
+                $this->addFlash('error', 'There was an error updating your settings. Please check the form and try again.');
+            }
+        }
 
         return $this->render('user_settings/index.html.twig', [
             'user' => $user,
-            'section' => $section
+            'section' => $section,
+            'form' => $form,
         ]);
     }
 }
