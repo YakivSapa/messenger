78f1550865db354d7f8ecd246ba4607423387bff
Yakiv <sapa.yakiv@gmail.com>
2026-06-12 12:02:05 +0200

    Implement password reset functionality in user settings

--- src/Controller/UserSettingsController.php
+++ src/Controller/UserSettingsController.php
@@ -10,18 +10,21 @@ use App\Form\GeneralUserSettingsType;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\User;
+use App\Controller\ResetPasswordController;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
 
 final class UserSettingsController extends AbstractController
 {
     #[Route('/user/settings/{section}', name: 'app_user_settings')]
-    public function index(Security $security, Request $request, EntityManagerInterface $entityManager, string $section = 'initial'): Response
+    public function index(Security $security, Request $request, EntityManagerInterface $entityManager, ResetPasswordController $resetPasswordController, MailerInterface $mailer, TranslatorInterface $translator, string $section = 'initial'): Response
     {
         /**
          * @var User $user
          */
         $user = $this->getUser();
         $currentUser = $security->getUser();
-        $form = $this->createForm(GeneralUserSettingsType::class);
         if (!$currentUser) {
             throw $this->createAccessDeniedException('You must be logged in to access this page.');
         }
@@ -30,6 +33,7 @@ final class UserSettingsController extends AbstractController
             throw $this->createNotFoundException('The requested settings section does not exist.');
         }
         if ($section === 'general') {
+            $form = $this->createForm(GeneralUserSettingsType::class);
             $form->handleRequest($request);
             if ($form->isSubmitted() && $form->isValid()) {
                 // dd($form->getData());
@@ -43,11 +47,21 @@ final class UserSettingsController extends AbstractController
                 $this->addFlash('error', 'There was an error updating your settings. Please check the form and try again.');
             }
         }
+        if ($section === 'password-reset') {
+            // $form = $this->;
+            // $form->handleRequest($request);
+            if ($request->isMethod('POST')) {
+                // dd($form->getData());
+
+                $this->addFlash('success', 'A password reset email has been sent to your email address. Please check your inbox and follow the instructions to reset your password.');
+                return $resetPasswordController->processSendingPasswordResetEmailFromSettings($user, $mailer);
+            }
+        }
 
         return $this->render('user_settings/index.html.twig', [
             'user' => $user,
             'section' => $section,
-            'form' => $form,
+            'form' => $form ?? null,
         ]);
     }
 }
