a3aa6328448964c351eea3bc152d503ae21d873e
Yakiv <sapa.yakiv@gmail.com>
2026-07-03 17:44:12 +0200

    Refactor CommunitySearchUserController to include FriendRequestRepository and handle pending requests

--- src/Controller/CommunitySearchUserController.php
+++ src/Controller/CommunitySearchUserController.php
@@ -2,29 +2,41 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
+use App\Repository\FriendRequestRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Repository\UserRepository;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use App\Entity\User;
 
 final class CommunitySearchUserController extends AbstractController
 {
     #[Route('/community/search/user', name: 'app_community_search_user', methods: ['GET'])]
-    public function index(Request $request, UserRepository $userRepository, #[CurrentUser] ?User $currentUser): Response
+    public function index(Request $request, UserRepository $userRepository, FriendRequestRepository $friendRequestRepository, #[CurrentUser] User $currentUser): Response
     {
         $query = $request->query->get('q', '');
         $users = [];
+        $pendingRequests = [];
 
-        if (!empty($query) && strlen($query) >= 2) {
+        if (strlen($query) >= 2) {
             $users = $userRepository->searchUsers($query);
+            $pendingRequests = $friendRequestRepository->getPendingRequestsForUserAsArray(
+                $currentUser
+            );
         }
+
+        // foreach ($users as $user){
+        //     $pendingRequests[$user->getId()] = $friendRequestRepository->hasPendingRequest($this->getUser(), $user);
+        // }
+        // dd($pendingRequests);
+        
         return $this->render('community_search_user/index.html.twig', [
             'users' => $users,
-            'searchQuery' => $query,
-            'currentUser' => $currentUser
+            'q' => $query,
+            'pendingRequests' => $pendingRequests,
+            'currentUser' => $currentUser,
         ]);
     }
 }
